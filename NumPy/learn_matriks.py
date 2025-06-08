80612f456435af52b43fefb403c122c6a5583d3a
Lesmana-cpu <lesmanadarajat42@gmail.com>
2025-06-08 10:14:13 +0700

    update

---
+++ NumPy/learn_matriks.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+matrix_a = np.array(([1, 2],
+                     [3, 4]))
+print(f"matrix_a = \n{matrix_a}")
+
+matrix_b = np.array(([5, 6],
+                     [7, 8]))
+print(f"matrix_b = \n{matrix_b}")
+
+c = np.dot(matrix_a, matrix_b)
+print(f"hasil matrix_a x matrix_b =\n{c}")
+
+x = np.array(([5,6,7],
+             [15,67,98]))
+
+y = np.array(([987,678],
+              [521,777],
+              [765,876]))
+
+z = np.dot(x,y)
+print(f"hasil matrix x kali y =\n{z}")
+
+
