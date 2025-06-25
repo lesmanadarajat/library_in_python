1d328fb877a73f3cbd886853dc620e62fe6d414e
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-06-25 14:48:28 +0700

    upload files

--- NumPy/learn_matriks.py
+++ NumPy/learn_matriks.py
@@ -3,7 +3,7 @@ import numpy as np
 matrix_a = np.array(([1, 2],
                      [3, 4]))
 print(f"matrix_a = \n{matrix_a}")
-
+ 
 matrix_b = np.array(([5, 6],
                      [7, 8]))
 print(f"matrix_b = \n{matrix_b}")
