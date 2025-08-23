d88bbe353e6a7fdb3c0235876a4ca6ebab479bfa
Lesmana Darajat <lesmanadarajat42@gmail.com>
2025-08-23 17:24:27 +0700

    upload files

---
+++ Pandas/data_analyze.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+pd.options.display.max_rows = 99999
+
+read_data = pd.read_csv('ai_human.csv')
+# print(read_data)
+
+
+print('ini head\n{}'.format(read_data.head()))
+
+print(f'ini tail\n{read_data.tail()}')
+
+print(read_data.info())
+
+print(pd.options.display.max_rows)
